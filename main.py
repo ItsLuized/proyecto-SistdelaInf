2932cf415025791122231fca6051333723374719
ItsLuized <luisemartinezcruz@gmail.com>
2019-10-25 11:42:35 -0500

    Updating templates and static files

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("home.html")
-
-@app.route("/login")
-def board():
-    return render_template("login.html")
+    return render_template('prueba1.html')
+    
+@app.route('/login',  methods = ['POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['email'] != 'admin@correo.com' or request.form['contraseña'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return "<h1> Lo lograste prro :V</h1>"
+    return render_template('prueba1.html', error=error)
     
 if __name__ == "__main__":
     app.run(debug=True)
